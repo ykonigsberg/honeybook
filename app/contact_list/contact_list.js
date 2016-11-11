61917bd8ec8ecbaac3974269a573d7c49a25e30a
Yosi Konigsberg <yoseph.konigsberg@gmail.com>
2016-11-11 18:47:00 +0200

    added request service, contat_item sekelton

---
+++ app/contact_list/contact_list.js
@@ -0,0 +1,48 @@
+'use strict';
+
+angular.module('contactList', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'contact_list/contact_list.html',
+    controller: 'ContactListCtrl'
+  });
+}])
+
+.controller('ContactListCtrl', ['$scope', 'contactService',
+    function($scope, contactService) {
+
+  /**
+   * Array of contacts.
+   * @type {Array<Object>}
+   */
+  $scope.contacts = [];
+
+  /**
+   * Flag for data response status.
+   * @type {boolean}
+   */
+  $scope.isDataReady = false;
+
+  /**
+   * Set the contacts data.
+   */
+  this.setContactList = function() {
+    contactService.getContactList()
+        .success(function(data) {
+          $scope.contacts = data;
+          $scope.isDataReady = true;
+          console.log("success");
+        }).error(function(data, status) {
+          console.error('error', status, data);
+        }).finally(function() {
+          console.log("finished");
+        });
+  };
+
+  //this.setContactList();
+}])
+
+.directive('contactItem', [function(){
+
+}]);
---
+++ app/services/contact_service.js
@@ -0,0 +1,8 @@
+angular.module('myApp').service('contactService', function($http) {
+    this.http_ = $http;
+    this.pathPrefix = 'https://candidate-test.herokuapp.com/';
+
+	this.getContactList = function() {
+        return this.http_ .get(this.pathPrefix + 'contacts');
+    };
+});
